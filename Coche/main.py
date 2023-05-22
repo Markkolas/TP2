1b212721dfd902884f092c6429feb06ca2489ff0
Debian Mark <>
2023-05-22 17:03:17 +0200

    Fusion metodo local y remoto, control via argumento '-clould'

--- Coche/main.py
+++ Coche/main.py
@@ -16,12 +16,20 @@ import threading
 from Funciones import *
 from Argumentos import *
 
+###VARIABLES GLOBALES###
 server_address = ('10.0.128.174',20001)
 
 control_acelerador=0
 control_giro=0
+
 datos = None
 
+do = threading.Event() #Do processing
+lock = threading.Lock()
+imagen_procesar = None
+#######################
+
+###CONTROL DE COCHE###
 def on_press(key):
     global control_acelerador
     global control_giro
@@ -67,49 +75,74 @@ listener.start()
 
 def send_control(control_giro,control_acelerador,address):
     sock.sendto(struct.pack('c',bytes('C','ascii'))+struct.pack('d',round(control_giro,3))+struct.pack('d',round(control_acelerador,3)),address)
+###############################
 
+###PROCESADO IMAGEN###
+#REMOTO
 def send_request_thread(modelo, traqueo, image_bytes):
     global datos
-    datos = send_request(file_list=[np.frombuffer(image_bytes, dtype=np.uint8)], model_name = modelo, tracking = traqueo)
-    
-def local_thread(modelo, image_bytes):
-    global datos
-    datos = local(imagen=[np.frombuffer(image_bytes, dtype=np.uint8)], model_name = modelo)
-    print("Si que entra\n", datos)
+    local_data = send_request(file_list=[np.frombuffer(image_bytes, dtype=np.uint8)], model_name = modelo, tracking = traqueo)
 
+    lock.acquire()
+    datos = local_data
+    lock.release()
+
+#LOCAL
+def procesado_imagen():
+    modelo_clasificador = torch.hub.load('ultralytics/yolov5', 'custom', path='./modelos/smallFinal.pt', force_reload=True)
+    global imagen_procesar
+    global datos
+
+    while True:
+        if do.is_set():
+            img = imagen_procesar
+            results = modelo_clasificador(img)
+            local_data = results_to_json(results, modelo_clasificador)
+
+            lock.acquire()
+            datos = local_data
+            lock.release()
+            do.clear()
+        else:
+            time.sleep(1)
+#######################
+
+###MAIN###
 if __name__ == "__main__":
-    
     args = parse_opt()
+
+    muestreo = args.muestreo
+    conteo = muestreo
+    received_payload=b''
 
     # Si se proporciona el argumento -help, imprime la ayuda y finaliza
     if args.help:
         print_help()
         exit()
-        
+
     if args.modelName:
         model_name = args.modelName
     else:
         print("\nNo has seleccionado el modelo\nEscribe python3 main.py -modelName <modelo>")
         exit()
-        
+
     if args.tracking:
         traqueo = True
     else:
         traqueo = False
 
-    firstLaunch = 1
-    muestreo = args.muestreo
-    conteo = muestreo
-    received_payload=b''
-    
-    
+    if not args.cloud:
+        do.clear()
+        thread = threading.Thread(target=procesado_imagen, args=())
+        thread.start()
+
     if not args.webcam:
         #auto_utils=artemis_autonomous_car.artemis_autonomous_car([0])
         #Generacion de socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         sock.bind(server_address)
-    
+
         #try:
             # Receive the data in small chunks and retransmit it
         while True:
@@ -120,107 +153,86 @@ if __name__ == "__main__":
             #print("Tipo de dato: ",data_type)
             received_payload=bytes(data[1:])
             data = pickle.loads(received_payload,encoding='latin1')
+
             if data_type == b'I':
                 img=cv2.imdecode(data,1)
-                img = cv2.resize(img, None, None, fx=1.5, fy=1.5)
+                #img = cv2.resize(img, None, None, fx=1.5, fy=1.5) DANGER
                 if conteo == muestreo:
-                    # Convert the image to bytes
-                    ret, buffer = cv2.imencode('.jpg', img)
-                    image_bytes = np.array(buffer).tobytes()
                     if args.cloud:
-                        if firstLaunch == 1:
+                        # Convert the image to bytes
+                        ret, buffer = cv2.imencode('.jpg', img)
+                        image_bytes = np.array(buffer).tobytes()
+
+                        try:
+                            if not thread.is_alive():
+                                thread = threading.Thread(target=send_request_thread, args=(model_name, traqueo, image_bytes,))
+                                thread.start()
+                        except:
                             thread = threading.Thread(target=send_request_thread, args=(model_name, traqueo, image_bytes,))
                             thread.start()
-                            firstLaunch = 0
-                        elif not thread.is_alive():
-                            thread = threading.Thread(target=send_request_thread, args=(model_name, traqueo, image_bytes,))
-                            thread.start()
-                    else:
-                        if firstLaunch == 1:
-                            thread = threading.Thread(target=local_thread, args=(model_name, image_bytes,))
-                            thread.start()
-                            firstLaunch = 0
-                        elif not thread.is_alive():
-                            thread = threading.Thread(target=local_thread, args=(model_name, image_bytes,))
-                            thread.start()  
+
+                    elif not do.is_set:
+                        imagen_procesar = img
+                        do.set()
 
                     conteo = 0
                 else:
                     conteo += 1
 
+                lock.acquire()
                 imagen = dibujar_caja(img, datos)
+                lock.release()
+
+                imagen = cv2.resize(imagen, None, None, fx=1.5, fy=1.5)
                 cv2.imshow("Coche ARTEMIS", imagen)
                 cv2.waitKey(1)
                 send_control(control_giro,control_acelerador,address)
+
             if data_type == b'D':
                 pass
             if data_type == b'L':
-                img_lidar = np.zeros((480,640,3), np.uint8)
-                i=0
-                ranges=data
-                for range in ranges:
-                    Z=cmath.rect(range,math.radians(-i-90))
-                    i = i+1
-                    if Z.real != float('inf') and Z.real != float('-inf'):
-                        x=int(Z.real*70)+320
-                    else:
-                        x=0
-                    if Z.imag != float('inf') and Z.imag != float('-inf'):
-                        y=int(Z.imag*70)+240
-                    else:
-                        y=0
-                    cv2.circle(img_lidar,(x,y), radius=2, color=(0,0,255),thickness=2)
-                    
-                cv2.imshow("Mapa LIDAR",img_lidar)
-                cv2.waitKey(1)
                 pass
-        #except Exception:
-        #    pass   
-        #finally:
-            # Clean up the connection
-        #    pass#connection.close()
+
     else:
         # Crear la ventana donde se mostrará el video
         cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
-        
+
         cap = cv2.VideoCapture(0)
         while cap.isOpened(): # Si nuestra webcam esta activa
             ret, frame = cap.read() # En frame se encuentra la imagen
-            
-            frame = cv2.resize(frame, None, None, fx=1.5, fy=1.5)
-                
-            if conteo == muestreo:
-                # Convert the image to bytes
-                ret, buffer = cv2.imencode('.jpg', frame)
-                image_bytes = np.array(buffer).tobytes()
-                
-                if args.cloud:
-                    if firstLaunch == 1:
-                        thread = threading.Thread(target=send_request_thread, args=(model_name, traqueo, image_bytes,))
-                        thread.start()
-                        firstLaunch = 0
-                    elif not thread.is_alive():
-                        thread = threading.Thread(target=send_request_thread, args=(model_name, traqueo, image_bytes,))
-                        thread.start()
-                else:
-                    if firstLaunch == 1:
-                        thread = threading.Thread(target=local_thread, args=(model_name, image_bytes,))
-                        thread.start()
-                        firstLaunch = 0
-                    elif not thread.is_alive():
-                        thread = threading.Thread(target=local_thread, args=(model_name, image_bytes,))
-                        thread.start()  
 
-                conteo = 0
-            else:
-                conteo += 1
-                
-            imagen = dibujar_caja(frame, datos)
+            frame = cv2.resize(frame, None, None, fx=1.5, fy=1.5)
+
+            if conteo == muestreo:
+                    if args.cloud:
+                        # Convert the image to bytes
+                        ret, buffer = cv2.imencode('.jpg', img)
+                        image_bytes = np.array(buffer).tobytes()
+
+                        try:
+                            if not thread.is_alive():
+                                thread = threading.Thread(target=send_request_thread, args=(model_name, traqueo, image_bytes,))
+                                thread.start()
+                        except:
+                            thread = threading.Thread(target=send_request_thread, args=(model_name, traqueo, image_bytes,))
+                            thread.start()
+
+                    elif not do.is_set:
+                        imagen_procesar = img
+                        do.set()
+
+                    conteo = 0
+                else:
+                    conteo += 1
+
+            lock.acquire()
+            imagen = dibujar_caja(img, datos)
+            lock.release()
             cv2.imshow("Video", imagen)
 
             # Esperar un poco antes de mostrar la siguiente imagen
             cv2.waitKey(1)
-            
+
             if cv2.waitKey(1) & 0xFF == ord('q'): # Para parar se le da a la 'q'
                 break
         cap.release()
