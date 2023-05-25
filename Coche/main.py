7363cb3d9d2caf97bd79b3805718b8ca48d990af
MrShirker <shirker97@gmail.com>
2023-05-25 18:58:10 +0200

    Añadido modelo onnx

--- Coche/main.py
+++ Coche/main.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
 
     if not args.cloud:
         do.clear()
-        modelo_clasificador = torch.hub.load('ultralytics/yolov5', 'custom', path='./modelos/smallFinal.pt', force_reload=True)
+        modelo_clasificador = torch.hub.load('ultralytics/yolov5', 'custom', path='./modelos/Final.onnx', force_reload=True)
         #modelo_clasificador = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
         thread = threading.Thread(target=procesado_imagen, args=(modelo_clasificador,))
         thread.start()
@@ -247,8 +247,9 @@ if __name__ == "__main__":
                 lock.release()
 
                 imagen = cv2.resize(imagen, None, None, fx=1.5, fy=1.5)
-                cv2.putText(imagen, "T. proc: "+f'{timer*1000:.2f}'+"ms", (20,650), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
-                cv2.putText(imagen, "FPS: "+f'{FPS:.2f}', (20,690), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
+                cv2.putText(imagen, "RTT: "+f'{timer*1000:.2f}'+"ms", (20,650), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
+                cv2.putText(imagen, "T. proc: "+datos[1]+"ms", (20,690), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
+                cv2.putText(imagen, "FPS: "+f'{FPS:.2f}', (20,730), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
                 cv2.imshow("Coche ARTEMIS", imagen)
                 cv2.waitKey(1)
                 send_control(control_giro,control_acelerador,address)
@@ -296,16 +297,18 @@ if __name__ == "__main__":
             imagen = dibujar_caja(img.copy(), datos)
             timer = ptime
             TOC = time.perf_counter()
-            if TOC - TIC > 10:
+            if TOC - TIC > 2:
                 FPS = fps_count/(TOC - TIC)
                 fps_count = 0
                 TIC = time.perf_counter()
             lock.release()
 
             imagen = cv2.resize(imagen, None, None, fx=1.5, fy=1.5)
-            cv2.putText(imagen, "T. proc: "+f'{timer*1000:.2f}'+"ms", (20,650), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
-            cv2.putText(imagen, "FPS: "+f'{FPS:.2f}', (20,690), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
-                
+            cv2.putText(imagen, "RTT: "+f'{timer*1000:.2f}'+"ms", (20,650), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
+            if not datos == None:
+                if len(datos) > 1:
+                    cv2.putText(imagen, "T. proc: "+datos[1]+"ms", (20,690), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
+            cv2.putText(imagen, "FPS: "+f'{FPS:.2f}', (20,610), cv2.FONT_HERSHEY_SIMPLEX, 1, (127,0,255), 2, cv2.LINE_AA)
             cv2.imshow("Coche ARTEMIS", imagen)
             cv2.waitKey(1)
 
