0e42cf2583f835eaa71a1d35603ec1704d386197
MrShirker <shirker97@gmail.com>
2023-06-01 11:22:16 +0200

    Cambio a la interfaz

--- Coche/Funciones.py
+++ Coche/Funciones.py
@@ -12,7 +12,7 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 def box_color():
-    # Colores para dibujar las cajas
+    ''' Colores para dibujar las cajas'''
     colors = [
         (31, 119, 180),   (255, 127, 14),    (44, 160, 44),    (214, 39, 40),
         (148, 103, 189),  (140, 86, 75),     (227, 119, 194),  (127, 127, 127),
@@ -47,8 +47,7 @@ def box_color():
     return colors
 
 def plot_one_box(x, im, color=(128, 128, 128), label=None, confidence=None, tracker_id=None, line_thickness=2):
-    # Función cogida del repositorio del repositorio de Ultralytics
-    # Plots one bounding box on image 'im' using OpenCV
+    ''' Función cogida del repositorio del repositorio de Ultralytics'''
     assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to plot_on_box() input image.'
     tl = line_thickness or round(0.002 * (im.shape[0] + im.shape[1]) / 2) + 1  # line/font thickness
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
@@ -65,7 +64,8 @@ def plot_one_box(x, im, color=(128, 128, 128), label=None, confidence=None, trac
         cv2.putText(im, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 def send_help():
-    # Enviamos la solicitud GET al punto final /custom_models
+    ''' Enviamos la solicitud GET al punto final /custom_models'''
+    
     res = r.get("http://143.47.38.177/custom_models")
     
     if res.ok and res.status_code == 200:
@@ -78,23 +78,12 @@ def send_help():
         return data
     else:
         print("Error al enviar la solicitud:", res.status_code)
-        
-def t_proc():
-    # Enviamos la solicitud GET al punto final /custom_models
-    res = r.get("http://143.47.38.177/t_proc")
-    
-    if res.ok and res.status_code == 200:
-       # Obtenemos los datos JSON de la respuesta
-        data = res.json()
-        media = data["media"]
-        return media
-    else:
-        print("Error al enviar la solicitud:", res.status_code)
 
 def send_request(file_list = [], 
                     model_name = 'yolov5s',
                     img_size = 640,
                     tracking = False):
+    ''' Esta función envia la solicitud a la nube, agregando la imagen y el nombre del modelo. Además si se quiere hacer traqueo hay que indicarlo.'''
     
     # Se podrían subir mas de una imagen a la vez, nosotros no hacemos uso de esta opción ya que enviamos una imagen en cada petición
     files = [('file_list', file) for file in file_list]
@@ -122,7 +111,7 @@ def send_request(file_list = [],
         print("Error al enviar la solicitud: ", res.status_code)
         
 def dibujar_caja(imagen, datos):
-    # Dibuja todas las cajas en una imagen
+    ''' Dibuja todas las cajas en una imagen'''
     
     colors = box_color()
 
@@ -141,59 +130,8 @@ def dibujar_caja(imagen, datos):
     else:
         return imagen
     
-def local(imagen, model_name, img_size=None):
-    
-    print(type(imagen[0]))
-    model_selection_options = ['yolov5s','yolov5m','yolov5l','yolov5x','yolov5n',
-                            'yolov5n6','yolov5s6','yolov5m6','yolov5l6','yolov5x6']
-    model_dict = {model_name: None for model_name in model_selection_options} #set up model cache
-
-    # Obtener la lista de nombres de archivos en la carpeta
-    file_names = os.listdir('modelos/')
-    # Extensiones de modelos válidas
-    valid_extensions = ['.torchscript', '.onnx', '_openvino_model', '.engine ', '.mlmodel', '_saved_model', '.pt', '.tflite', '_edgetpu.tflite', '_paddle_model ']
-    
-    # Filtrar solo los archivos con extensiones de modelos válidas
-    model_names = [file_name for file_name in file_names if os.path.splitext(file_name)[1] in valid_extensions]
-    # Crear el diccionario con los nombres de los modelos
-    custom_model_dict = {model_name: None for model_name in model_names}
-
-    if model_name in custom_model_dict:
-        custom_model = True
-    else:
-        custom_model = False
-        
-    if model_name in model_dict:
-        yolo_model = True
-    else:
-        yolo_model = False
-    
-    print(model_dict)
-    print(custom_model_dict)
- 
-    if custom_model:
-        if custom_model_dict[model_name] is None:
-            custom_path = 'modelos/'+model_name
-            print(custom_path)
-            for clave in custom_model_dict.keys():
-                print(clave)
-            custom_model_dict[model_name] = torch.hub.load('ultralytics/yolov5', 'custom',  path=custom_path)
-        results = custom_model_dict[model_name](imagen, size = img_size) 
-        json_results = results_to_json(results,custom_model_dict[model_name])
-    elif yolo_model:
-        if model_dict[model_name] is None:
-            model_dict[model_name] = torch.hub.load('ultralytics/yolov5', model_name, pretrained=True)
-        results = model_dict[model_name](imagen, size = img_size) 
-        json_results = results_to_json(results,model_dict[model_name])
-    else:
-        print("El modelo elegido no esta disponible")
-        exit()
-        
-    encoded_json_results = str(json_results).replace("'",r'"')
-    return encoded_json_results
-
 def results_to_json(results, model):
-    ''' Converts yolo model output to json (list of list of dicts)'''
+    ''' Convierte la salida del modelo yolo en una lista de listas de diccionarios'''
     return [
                 [
                     {
@@ -208,6 +146,7 @@ def results_to_json(results, model):
             ]
 
 def add_text_to_image(image, timer, datos, FPS):
+    ''' Funcion que impllemeta la interfaz grafica de las metricas de velocidad del coche'''
     # Crear una imagen en blanco con el mismo tamaño que la imagen original
     overlay = np.zeros_like(image)
 
@@ -229,19 +168,17 @@ def add_text_to_image(image, timer, datos, FPS):
     text_y = rect_y + text_height + 10
     
     # Dibujar el rectángulo de fondo en la capa de superposición
-    cv2.rectangle(overlay, (rect_x, 0), (rect_x + rect_width, rect_y + rect_height), (100, 100, 100), -1)
-    
     cv2.rectangle(overlay, (rect_x, 0), (rect_x + rect_width, rect_y + rect_height), (50, 50, 50), thickness=3, lineType=cv2.LINE_AA)
     
     # Agregar el texto a la capa de superposición
    
     if len(datos) > 1:
-        cv2.putText(overlay, "FPS: " + f'{FPS:.2f}', (text_x, text_y - text_height - 10), font, font_scale, (1, 1, 1), text_thickness, line_type)
-        cv2.putText(overlay, "RTT: " + f'{timer*1000:.2f}' + "ms", (text_x, text_y), font, font_scale, (1, 1, 1), text_thickness, line_type)
-        cv2.putText(overlay, "T. proc: " + datos[1] + "ms", (text_x, text_y + text_height + 10), font, font_scale, (1, 1, 1), text_thickness, line_type)
+        cv2.putText(overlay, "FPS: " + f'{FPS:.2f}', (text_x, text_y - text_height - 10), font, font_scale, (255, 20, 147), text_thickness, line_type)
+        cv2.putText(overlay, "RTT: " + f'{timer*1000:.2f}' + "ms", (text_x, text_y), font, font_scale, (255, 20, 147), text_thickness, line_type)
+        cv2.putText(overlay, "T. proc: " + datos[1] + "ms", (text_x, text_y + text_height + 10), font, font_scale, (255, 20, 147), text_thickness, line_type)
     else:
-        cv2.putText(overlay, "FPS: " + f'{FPS:.2f}', (text_x, text_y - text_height), font, font_scale, (1, 1, 1), text_thickness, line_type)
-        cv2.putText(overlay, "T. proc: " + f'{timer*1000:.2f}' + "ms", (text_x, text_y + text_height), font, font_scale, (1, 1, 1), text_thickness, line_type)
+        cv2.putText(overlay, "FPS: " + f'{FPS:.2f}', (text_x, text_y - text_height), font, font_scale, (255, 20, 147), text_thickness, line_type)
+        cv2.putText(overlay, "T. proc: " + f'{timer*1000:.2f}' + "ms", (text_x, text_y + text_height), font, font_scale, (255, 20, 147), text_thickness, line_type)
     
     # Fusionar la capa de superposición con la imagen original
     image_with_text = cv2.add(image, overlay)
